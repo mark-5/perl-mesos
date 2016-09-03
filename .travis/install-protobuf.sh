ab56952f7e2e2fd4e8aae935dc5b9a6981000a3b
mark-5 <maf@cpan.org>
2016-09-03 02:51:10 +0000

    use docker-compose with travis-ci

--- .travis/install-protobuf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-VERSION=2.6.0
-
-wget https://github.com/google/protobuf/releases/download/v$VERSION/protobuf-$VERSION.tar.gz
-tar -zxvf protobuf-$VERSION.tar.gz
-cd protobuf-$VERSION && ./configure --prefix=/usr && make && sudo make install
